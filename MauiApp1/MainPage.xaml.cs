e321d494190b6c04149795ca12e806007879b3bd
Даня Ширлин <sirlindaniil@gmail.com>
2026-01-20 17:14:03 +0300

    Добавьте файлы проекта.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Maui.Controls;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Transactions;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnIncomeButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new IncomePage());
+        }
+
+        private async void OnExpenseButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ExpensePage());
+        }
+
+        private async void OnReportButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ReportPage());
+        }
+    }
+
+}
---
+++ MauiApp1/Transaction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public class Transaction
+    {
+        public string CategoryIcon { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public string Date { get; set; }
+    }
+
+
+}
